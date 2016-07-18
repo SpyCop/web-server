54aba7b43a81704c323959c3d7760a0d47188704
SpyCop <g.c.vandenbrink@student.utwente.nl>
2016-07-18 23:30:10 +0200

    Relocated middleware to seperate file

--- server.js
+++ server.js
@@ -2,22 +2,13 @@ var express = require('express');
 var app = express();
 var PORT = 3000;
 
-var middleware = {
-	requireAuthentication: function (req, res, next) {
-		console.log('Private route hit!');
-		next();
-	}, 
-	logger: function(req, res, next) {
-		console.log('Request: '+ new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
-		next();
-	}
-};
+var middleware = require(__dirname + '/middleware.js');
 
 //app.use(middleware.requireAuthentication);
 app.use(middleware.logger);
 
 app.get('/about', middleware.requireAuthentication, function (req, res) {
-	res.send('About us, yeah boi');
+	res.send('About us');
 });
 
 app.use(express.static(__dirname+ '/public'));
